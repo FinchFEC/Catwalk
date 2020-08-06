aaf2aca8f0d2eff0a218075a9a5efcd43196a7cc
xdynasty <59094628+xdynasty@users.noreply.github.com>
2020-08-06 15:51:08 -0400

    fixed typo: added parenthesis

--- src/client/redux/store/index.js
+++ src/client/redux/store/index.js
@@ -2,6 +2,6 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
 
 export default store;
