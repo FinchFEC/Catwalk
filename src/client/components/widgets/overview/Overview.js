151d128f41773ed6d119b9fa528f4068376174c5
Anna Makagonova <makagonova.ann@gmail.com>
2020-08-07 15:10:13 -0400

    add grid and styles

--- src/client/components/widgets/overview/Overview.js
+++ src/client/components/widgets/overview/Overview.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "../../../assets/scss/overview.scss";
+import PhotoCarousel from "./PhotoCarousel.js";
+import CurrentPhoto from "./CurrentPhoto.js";
 
 const Overview = () => {
   return (
@@ -10,6 +12,12 @@ const Overview = () => {
           <a href="">NEW PRODUCT HIGHLIGHT</a>{" "}
         </p>
       </header>
+      <div>
+        <PhotoCarousel />
+      </div>
+      <div>
+        <CurrentPhoto />
+      </div>
     </div>
   );
 };
--- src/client/components/widgets/overview/PhotoCarousel.js
+++ src/client/components/widgets/overview/PhotoCarousel.js
@@ -1,2 +1,8 @@
 import React from "react";
 import "../../../assets/scss/overview.scss";
+
+const PhotoCarousel = () => {
+  return <div className="photo-carousel"></div>;
+};
+
+export default PhotoCarousel;
