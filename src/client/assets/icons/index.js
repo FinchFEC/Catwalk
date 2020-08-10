7232373e2234e66628e9efe5f9c682abe37e50ed
xdynasty <xdynasty5201314@gmail.com>
2020-08-10 19:56:11 -0400

    edit fort-awesome instructions

--- src/client/assets/icons/index.js
+++ src/client/assets/icons/index.js
@@ -16,12 +16,12 @@ import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 // add icons to library
 library.add(faStar, fasStar, faTimesCircle, faCheckCircle, faCheck, faFacebook);
 
-// Instructions (https://www.npmjs.com/package/@fortawesome/react-fontawesome)
-// 1. browse for icons on https://fontawesome.com/
+/* *************INSTRUCTIONS (https://www.npmjs.com/package/@fortawesome/react-fontawesome)************* */
+// 1. browse icons on https://fontawesome.com/
 // 2. import icons from respective @fortawesome npm dependencies
 // 3. add icons to library with library.add()
 // 4. inside your react component, import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // 5. inside your react component, import '../../../assets/icons';
 // 6. inside your react component, use icons with <FontAwesomeIcon icon={['replaceWithFontAwesomeStyleCode', 'replace-with-icon-code']}/>
 // style code: one of "fab", "fas", "far"
-// icon-code: hyphen-cased
+// icon-code: lowercase-hyphen-cased code from https://fontawesome.com/ (don't include the style code in the icon code)
