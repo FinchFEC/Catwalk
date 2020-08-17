e87ad96a3080bf5141d40a32176bf7828b2db851
xdynasty <xdynasty5201314@gmail.com>
2020-08-16 23:54:13 -0400

    set noMoreReviews to false after changing sort or productId and add username input value to AddReviewModal

--- src/client/redux/reducers/noMoreReviews.js
+++ src/client/redux/reducers/noMoreReviews.js
@@ -2,6 +2,9 @@ function noMoreReviews(state = false, action) {
   switch (action.type) {
     case 'NO_MORE_REVIEWS':
       return true;
+    case 'CHANGE_SORT':
+    case 'CHANGE_PRODUCT_ID':
+      return false;
     default:
       return state;
   }
